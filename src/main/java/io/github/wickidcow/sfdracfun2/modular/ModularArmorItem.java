118133f8169f8d38194150b6bc00a43b772eb2ad
wickidcow <jaywago@gmail.com>
2026-09-17 18:04:54 -0400

    Add modern modular armor protection type

---
+++ src/main/java/io/github/wickidcow/sfdracfun2/modular/ModularArmorItem.java
@@ -0,0 +1,33 @@
+package io.github.wickidcow.sfdracfun2.modular;
+
+import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import io.github.thebusybiscuit.slimefun4.core.attributes.ProtectiveArmor;
+import io.github.thebusybiscuit.slimefun4.core.attributes.ProtectionType;
+import javax.annotation.Nonnull;
+import org.bukkit.NamespacedKey;
+
+/** Modular armor with the protection contract exposed by DracFun 2.0.10. */
+public final class ModularArmorItem extends ModularGearItem implements ProtectiveArmor {
+
+    private static final NamespacedKey ARMOR_SET_ID = LegacyDracFunKeys.key("DRACFUN_ARMOR");
+
+    public ModularArmorItem(ItemGroup group, SlimefunItemStack item, int gearTier) {
+        super(group, item, GearType.ARMOR, gearTier);
+    }
+
+    @Override
+    public @Nonnull ProtectionType[] getProtectionTypes() {
+        return ProtectionType.values();
+    }
+
+    @Override
+    public boolean isFullSetRequired() {
+        return false;
+    }
+
+    @Override
+    public NamespacedKey getArmorSetId() {
+        return ARMOR_SET_ID;
+    }
+}
