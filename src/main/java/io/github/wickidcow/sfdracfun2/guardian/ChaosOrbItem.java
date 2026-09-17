8d6691289fd8afd7ece02d859a2d86aef53b4389
wickidcow <jaywago@gmail.com>
2026-09-17 19:43:26 -0400

    Add Chaos Orb invocation item

---
+++ src/main/java/io/github/wickidcow/sfdracfun2/guardian/ChaosOrbItem.java
@@ -0,0 +1,37 @@
+package io.github.wickidcow.sfdracfun2.guardian;
+
+import io.github.thebusybiscuit.slimefun4.api.events.PlayerRightClickEvent;
+import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
+import io.github.thebusybiscuit.slimefun4.core.attributes.NotPlaceable;
+import io.github.thebusybiscuit.slimefun4.core.handlers.ItemUseHandler;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * Clean-room Chaos Orb of Invocation.
+ *
+ * <p>The old addon consumed the orb before validating armor. Reborn deliberately
+ * validates the End battle and modular chestplate first, then consumes one orb
+ * only when an invocation was actually accepted.</p>
+ */
+public final class ChaosOrbItem extends SlimefunItem implements NotPlaceable, ItemUseHandler {
+
+    private final ChaosGuardianService guardianService;
+
+    public ChaosOrbItem(
+            ItemGroup itemGroup,
+            SlimefunItemStack item,
+            ChaosGuardianService guardianService) {
+        super(itemGroup, item, RecipeType.NULL, new ItemStack[9]);
+        this.guardianService = guardianService;
+        addItemHandler(this);
+    }
+
+    @Override
+    public void onRightClick(PlayerRightClickEvent event) {
+        event.cancel();
+        guardianService.invoke(event.getPlayer(), event.getItem());
+    }
+}
