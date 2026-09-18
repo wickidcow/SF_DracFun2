8f057e38fa1b5e6344ee7c69336d1679b3915f80
wickidcow <jaywago@gmail.com>
2026-09-18 19:45:23 -0400

    feat: show Fusion recipes on modular weapons

--- src/main/java/io/github/wickidcow/sfdracfun2/modular/ModularWeaponItem.java
+++ src/main/java/io/github/wickidcow/sfdracfun2/modular/ModularWeaponItem.java
@@ -2,13 +2,22 @@ package io.github.wickidcow.sfdracfun2.modular;
 
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.core.handlers.WeaponUseHandler;
 
 /** Sword/staff modular item with the legacy powered DAMAGE-module behavior. */
+import org.bukkit.inventory.ItemStack;
+
 public final class ModularWeaponItem extends ModularGearItem {
 
-    public ModularWeaponItem(ItemGroup group, SlimefunItemStack item, GearType gearType, int gearTier) {
-        super(group, item, gearType, gearTier);
+    public ModularWeaponItem(
+            ItemGroup group,
+            SlimefunItemStack item,
+            GearType gearType,
+            int gearTier,
+            RecipeType recipeType,
+            ItemStack[] recipe) {
+        super(group, item, gearType, gearTier, recipeType, recipe);
         if (gearType != GearType.SWORD && gearType != GearType.STAFF) {
             throw new IllegalArgumentException("ModularWeaponItem only supports sword/staff gear");
         }
