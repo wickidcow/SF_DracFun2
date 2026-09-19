15d42cbc63498c918a32f9793ee71f6173fc0e08
wickidcow <jaywago@gmail.com>
2026-09-18 22:08:37 -0400

    guide: give modular armor Fusion recipe metadata

--- src/main/java/io/github/wickidcow/sfdracfun2/modular/ModularArmorItem.java
+++ src/main/java/io/github/wickidcow/sfdracfun2/modular/ModularArmorItem.java
@@ -2,18 +2,25 @@ package io.github.wickidcow.sfdracfun2.modular;
 
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.core.attributes.ProtectiveArmor;
 import io.github.thebusybiscuit.slimefun4.core.attributes.ProtectionType;
 import javax.annotation.Nonnull;
 import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
 
 /** Modular armor with the protection contract exposed by DracFun 2.0.10. */
 public final class ModularArmorItem extends ModularGearItem implements ProtectiveArmor {
 
     private static final NamespacedKey ARMOR_SET_ID = LegacyDracFunKeys.key("DRACFUN_ARMOR");
 
-    public ModularArmorItem(ItemGroup group, SlimefunItemStack item, int gearTier) {
-        super(group, item, GearType.ARMOR, gearTier);
+    public ModularArmorItem(
+            ItemGroup group,
+            SlimefunItemStack item,
+            int gearTier,
+            RecipeType recipeType,
+            ItemStack[] recipe) {
+        super(group, item, GearType.ARMOR, gearTier, recipeType, recipe);
     }
 
     @Override
